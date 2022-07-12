739e563d850e3eb5ba79825744ba9f18f6ad4838
hema <yhema123@yahoo.com>
2022-07-12 23:30:00 +0530

    first commit

--- src/TestClass.java
+++ src/TestClass.java
@@ -4,6 +4,11 @@ public class TestClass {
 	public static void main(String[] args) {
 
 		System.out.println("I am the main master please clone me ");
+		
+		// created the projected 
+		// initialized it as git 
+		//git push -u origin 
+		// git push --set-upstream origin master
 	}
 
 }
